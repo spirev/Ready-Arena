0326e2b742d86ecfc8f2c236b83a7a79714feff2
spirev <prospirev@gmail.com>
2020-03-10 23:20:06 +0100

    html/css for LoginVIew and SignUpVIew

---
+++ controllers/LoginController.php
@@ -0,0 +1,8 @@
+<?php
+    define('ROOT_PATH', __DIR__);
+        
+    if (isset($_GET['path'])) {
+        $dataview = $_GET['path']."View/".$_GET['path']."View.phtml";
+        include ROOT_PATH.'/../views/layout.phtml';
+    }
+?>
