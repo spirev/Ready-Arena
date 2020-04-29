708d70aa061acfd546df4e3d1da14d94fc0cae64
spirev <prospirev@gmail.com>
2020-04-29 22:42:30 +0200

    Sign-Up ok

---
+++ controllers/addUser.php
@@ -0,0 +1,65 @@
+<?php
+    define('ROOT_PATH', __DIR__);
+    include ROOT_PATH.'/../library/bdd.class.php';
+    include ROOT_PATH.'/../models/UsersModel.class.php';
+
+    $usersModel = new UsersModel();
+    $varTest;
+
+    // if one field is not fill redirect to signup page
+    if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['password']) || !isset($_POST['confirmPWD'])) {
+        header('Location: SignUpController.php?path=SignUp&error=empty');
+        //exit;
+    }
+
+    // check if name field contain an unused name and has two character minimum
+    if (strlen($_POST['name']) >= 2) {
+        $varTest = $usersModel->checkName($_POST['name']);
+        if (!empty($varTest)) {
+            header('Location: SignUpController.php?path=SignUp&error=name');
+            exit;
+        }
+    }
+    else {
+        header('Location: SignUpController.php?path=SignUp&error=name');
+        exit;
+    }
+
+    //check if email field is a valid address format and is not already used
+    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $varTest = $usersModel->findByEmail($_POST['email']);
+        if (!empty($varTest)) {
+            header('Location: SignUpController.php?path=SignUp&error=email');
+            exit;
+        }
+    }
+    else {
+        header('Location: SignUpController.php?path=SignUp&error=email');
+        exit;
+    }
+
+    //check if password is correct (contain at least 8 characters / 1 number / 1 Uppercase
+    if (strlen($_POST['password']) >= 8) {
+        if (!preg_match('/[A-Z]/', $_POST['password']) || !preg_match('/[0-9]/', $_POST['password']) || !preg_match('/[a-z]/', $_POST['password'])) {
+            header('Location: SignUpController.php?path=SignUp&error=PWD');
+            exit;
+        }
+    }
+    else {
+        header('Location: SignUpController.php?path=SignUp&error=PWD');
+        exit;
+    }
+
+    //check if password confirmation match password
+    if (strcmp($_POST['confirmPWD'], $_POST['password'])) {
+        header('Location: SignUpController.php?path=SignUp&error=confirmPWD');
+        exit;
+    }
+
+    //this take the user in DB who has the lower amount of ladder_point and make current signing user the same rank
+    //there is a false user in DB whom stay at the lowerest rank for this to work
+    $varTest = $usersModel->lowerestPoint();
+    $usersModel->addUser($_POST['name'], $_POST['email'], $_POST['password'], $varTest[0]['rank']);
+    header('Location: ../index.php?flash=signUp');
+
+?>
--- models/UsersModel.class.php
+++ models/UsersModel.class.php
@@ -7,14 +7,11 @@
             $this->db = new Database();
         }
 
-        public function findAllByPoints() {
-            return $this->db->getAll("SELECT * FROM users ORDER BY ladder_point DESC");
-        }
-
+        
         public function findAll() {
             return $this->db->getAll("SELECT * FROM users");
         }
-
+        
         public function findById(int $id) {
             return $this->db->getAll("SELECT * FROM users WHERE id = '$id'");
         }
@@ -22,18 +19,26 @@
         public function findByEmail(string $email) {
             return $this->db->getAll("SELECT * FROM users WHERE email = '$email'");
         }
-
+        
         public function findByName() {
             return $this->db->getAll("SELECT * FROM users ORDER BY name ASC");
         }
+        
+        public function checkName($name) {
+            return $this->db->getAll("SELECT * FROM users WHERE name = '$name'");
+        }
+        
+        public function findAllByPoints() {
+            return $this->db->getAll("SELECT * FROM users ORDER BY ladder_point DESC");
+        }
 
-        public function loginVerify(string $email, string $password) {
-            $user = $this->db->getAll("SELECT * FROM users WHERE email = '$email'");
-            if(!empty($user)) {
-                if(isset($user['password']) && $user['password'] == $password) {
-                    return $user;
-                }
-            }
+        public function lowerestPoint() {
+            return $this->db->getAll("SELECT * FROM users WHERE ladder_point = 0");
+        }
+
+        public function addUser($name, $email, $password, $rank, $ladder_point = 0, $look_for_team = null) {
+            $this->db->insert("INSERT INTO users (id, name, email, password, ladder_point, rank, look_for_team)
+            VALUES (NULL, '$name', '$email', '$password', '$ladder_point', '$rank', '$look_for_team')");
         }
 
     }
