c53f61c0318c63d1cab74e0b509fbdde2f047506
spirev <prospirev@gmail.com>
2020-05-27 18:06:40 +0200

    responsive oneTeam + resize some game_icone + login hidden + some css change

--- controllers/onePlayer.php
+++ controllers/onePlayer.php
@@ -15,69 +15,48 @@
     $user = $usersModel->findById($_GET['id']);
     $allTournaments = $TournamentsModel->findAll();
     $allTeams = $TeamsModel->findAll();
-    $userIdLength = strlen($_GET['id']);
-// take "look_for_team" string in page user and explode it into an array for phmtl use
-    $seekOnGame = $user[0]['look_for_team'];
+
+    $lookingForTeam = $user[0]['look_for_team'];
     $list = [];
     $games = [];
-    $i = 0; /* REMOVE ? */
 
 // make list of LFT games
-    if(!empty($seekOnGame)) {
-        $list = explode(' ', $seekOnGame);
+    if(!empty($lookingForTeam)) {
+        $list = explode(' ', $lookingForTeam);
     }
     for($i = 0; $i < count($list);$i++) {
         $games[$i] = $gameModel->findById(intval($list[$i], 10));
     }
+
 // make a list of unset LFT games for '#seekTeam section' selection
 $notLFTGames = $gameModel->findAll();
 
 // take playerList from all tournaments and look for page user for phtml use (not smart / inner join expected)
     $tournaments = [];
-    $i = 0;
+    $y = 0;
 
-    for($i = 0;$i < count($allTournaments);$i++) {
-        $seekTournaments[$i] = $allTournaments[$i]['playerList'];
-    }
-    for ($i = 0;$i < count($seekTournaments);$i++) {
-        if (strpos(substr($seekTournaments[$i], 0, ($userIdLength)), $_GET['id']) !== false) {
-            $tournaments[$i] = $TournamentsModel->findById($i + 1);
-        }
-        else if (strpos($seekTournaments[$i], " ".$_GET['id']." ") !== false) {
-            $tournaments[$i] = $TournamentsModel->findById($i + 1);
-        }
-        else if(strpos(substr($seekTournaments[$i], ($userIdLength - 1)), " ".$_GET['id']) !== false) {
-            $tournaments[$i] = $TournamentsModel->findById($i + 1);
-        }
-        else {
+    for ($i = 0;$i < count($allTournaments);$i++) {
+        if (in_array($_GET['id'], explode(' ', $allTournaments[$i]['playerList']))) {
+            $tournaments[$y] = $TournamentsModel->findById($i + 1);
+            $y++;
         }
     }
 
 // take teammates from all teams and look for page user for phtml use (not smart / inner join expected)
     $teams = [];
-    $i = 0; /* REMOVE ? */
+    $y = 0; /* REMOVE ? */
 
-    for($i = 0;$i < count($allTeams);$i++) {
-        $seekTeams[$i] = $allTeams[$i]['teammates'];
-    }
-    for ($i = 0;$i < count($seekTeams);$i++) {
-            if (strpos(substr($seekTeams[$i], 0, ($userIdLength + 1)), $_GET['id']) !== false) {
-                $teams[$i] = $TeamsModel->findById($i + 1);
-            }
-            else if (strpos($seekTeams[$i], " ".$_GET['id']." ") !== false) {
-                $teams[$i] = $TeamsModel->findById($i + 1);
-            }
-            else if(strpos(substr($seekTeams[$i], ($userIdLength * -1 - 1)), " ".$_GET['id']) !== false) {
-                $teams[$i] = $TeamsModel->findById($i + 1);
-            }
-            else {
-            }
+    for ($i = 0;$i < count($allTeams);$i++) {
+        if (in_array($_GET['id'], explode(' ', $allTeams[$i]['teammates']))) {
+            $teams[$y] = $TeamsModel->findById($i + 1);
+            $y++;
+        }
     }
 
-    //take all invitations from teams to display them below looking for a team section on the left page
+//take all invitations from teams to display them under 'looking for a team' section on the left page
     if (!empty($user[0]['team_invite'])) {
-        $invitations = explode(' ', $user[0]['team_invite']);
         $invite_list = [];
+        $invitations = explode(' ', $user[0]['team_invite']);
         for($i = 0;$i < count($invitations);$i++) {
             $invite_list[$i] = $TeamsModel->findById($invitations[$i]);
         }
